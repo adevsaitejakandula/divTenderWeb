927a1ad0a6dbb89f930ab993dd7a1eaf4557a7e8
ADeveloperSivakumari <sivakumari2702>
2026-08-17 12:40:08 +0530

    updated base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:2326'
+// export const BASE_URL = 'http://localhost:2326'
+export const BASE_URL = location.hostname === "localhost" ? 'http://localhost:2326' : '/api'
